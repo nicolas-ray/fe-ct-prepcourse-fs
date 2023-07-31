194b22fe5870b04c02050e14cedc25579fd33853
nicolas.mray <nicolas.mray@gmail.com>
2023-07-30 21:22:15 -0300

    tarea

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -3,34 +3,40 @@
 
 function devolverString(string) {
    // Debe retornar un string.
-   // Tu código:
+   // Tu código: 
+   return string;
 }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
-   // Tu código:
+   // Tu código: 
+   return x+y;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
-   // Tu código:
+   // Tu código: function resta(x, y){console.log(x-y)}
+   return x-y;
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
-   // Tu código:
+   // Tu código: function divide(x, y) {x/y}
+   return x/y;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
-   // Tu código:
+   // Tu código: function multiplica(x, y) {x*y}
+   return x*y;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+   // Tu código: function obtenerResto(x, y) {x % y}
+   return x % y;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
